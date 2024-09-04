fa6f3e590a9c4413cfe049ea80b0ab9be5af4cdf
DEV_WOONG <106324609+dbsdndcks@users.noreply.github.com>
2024-09-04 20:25:26 +0900

    [Silver V] Title: 숫자 카드, Time: 1948 ms, Memory: 355820 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        HashMap<Integer, Boolean> map = new HashMap<>();
+        for (int i = 0; i < N; i++) {
+            map.put(sc.nextInt(), true);
+        }
+
+        int M = sc.nextInt();
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < M; i++) {
+            if (map.containsKey(sc.nextInt())) {
+                result.append("1 ");
+            } else {
+                result.append("0 ");
+            }
+        }
+
+        System.out.println(result.toString().trim());
+    }
+}
