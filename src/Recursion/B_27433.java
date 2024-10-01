327328348165b52c471a0f6e96af549d98ddea69
DEV_WOONG <ywcsuwon@gmail.com>
2024-10-01 19:38:55 +0900

    백준 재귀문제 해결

---
+++ src/Recursion/B_27433.java
@@ -0,0 +1,21 @@
+package Recursion;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B_27433 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        System.out.println(factorial(N));
+    }
+
+    static long factorial(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return n * factorial(n-1);
+    }
+}
