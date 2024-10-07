1023ee042b0b140e9ec5b883022d4ba8644ec6a2
DEV_WOONG <ywcsuwon@gmail.com>
2024-10-07 22:35:12 +0900

    백준 재귀 문제 칸토어 집합

--- src/Recursion/B_4779.java
+++ src/Recursion/B_4779.java
@@ -1,4 +1,30 @@
 package Recursion;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
 public class B_4779 {
+    static String[]arr;
+
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N;
+        int total;
+        while (true) {
+            N = Integer.parseInt(br.readLine());
+            total = (int) Math.pow(N);
+            arr = new String[total];
+            for (int i = 0; i < total; i++) {
+                arr[i] = br.readLine();
+            }
+            recursion(arr);
+        }
+    }
+
+    private static void recursion(String[] arr) {
+        
+    }
+
 }
+
