451787d03bece5ade667e03f19aed94a47b61089
DEV_WOONG <ywcsuwon@gmail.com>
2024-10-10 22:36:24 +0900

    Create B_15649.java

---
+++ src/Recursion/B_15649.java
@@ -0,0 +1,26 @@
+package Recursion;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B_15649 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = i+1;
+        }
+
+        sequence(arr, M);
+    }
+
+    private static void sequence(int[] arr, int m) {
+        
+    }
+}
