b910da434b8837efec1ea1162a0344610541db74
DEV_WOONG <ywcsuwon@gmail.com>
2024-11-18 14:50:51 +0900

    백준 24444 문제 해결

--- src/DFS_BFS/B_24444.java
+++ src/DFS_BFS/B_24444.java
@@ -1,4 +1,76 @@
 package DFS_BFS;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.util.Queue;
+import java.util.StringTokenizer;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.ArrayList;
+
 public class B_24444 {
+
+    static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+    static boolean[] visited;
+    static int[] result;
+    static int order = 1;
+    static Queue<Integer> queue = new LinkedList<>();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
+
+        visited = new boolean[N + 1];
+        result = new int[N + 1];
+        for (int i = 0; i <= N; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 1; i <= M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
+
+        for (int i = 1; i <= N; i++) {
+            Collections.sort(graph.get(i));
+        }
+
+        bfs(R);
+
+        for (int i = 1; i <= N; i++) {
+            bw.write(result[i] + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    public static void bfs(int node) {
+        queue.offer(node);
+        visited[node] = true;
+
+        while (!queue.isEmpty()) {
+            int x = queue.poll();
+            result[x] = order++;
+            for (int i = 0; i < graph.get(x).size(); i++) {
+                int y = graph.get(x).get(i);
+                if (!visited[y]) {
+                    queue.offer(y);
+                    visited[y] = true;
+                }
+            }
+        }
+    }
 }
