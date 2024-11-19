b8400a6576e39943f9ade4a451fd174cab5fae8e
DEV_WOONG <ywcsuwon@gmail.com>
2024-11-20 02:07:08 +0900

    백준 1260문제 재해결

--- src/DFS_BFS/B_1260.java
+++ src/DFS_BFS/B_1260.java
@@ -1,22 +1,14 @@
 package DFS_BFS;
+import java.io.*;
+import java.util.*;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.LinkedList;
-import java.util.StringTokenizer;
-import java.util.Collections;
-import java.util.Queue;
-import java.util.ArrayList;
-import java.io.IOException;
 public class B_1260 {
 
     static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-    static Queue<Integer> queue = new LinkedList<>();
-    static boolean[]visited;
-    static int dfsResult[];
-    static int bfsResult[];
+    static boolean[] dfsVisited;
+    static boolean[] bfsVisited;
+    static ArrayList<Integer> dfsResult = new ArrayList<>();
+    static ArrayList<Integer> bfsResult = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -27,9 +19,9 @@ public class B_1260 {
         int M = Integer.parseInt(st.nextToken());
         int V = Integer.parseInt(st.nextToken());
 
-        visited = new boolean[N + 1];
-        dfsResult = new int[N + 1];
-        bfsResult = new int[N + 1];
+        dfsVisited = new boolean[N + 1];
+        bfsVisited = new boolean[N + 1];
+
         for (int i = 0; i <= N; i++) {
             graph.add(new ArrayList<>());
         }
@@ -43,27 +35,59 @@ public class B_1260 {
             graph.get(v).add(u);
         }
 
-        bfs(V);
+        for (int i = 1; i <= N; i++) {
+            if (!graph.get(i).isEmpty()) {
+                Collections.sort(graph.get(i));
+            }
+        }
+
+        // DFS 탐색
         dfs(V);
 
-        for (int i = 1; i <= N; i++) {
-            Collections.sort(graph.get(i));
-        }
+        // BFS 탐색
+        bfs(V);
 
-        for (int i = 1; i <= N; i++) {
-            bw.write(dfsResult[i] + " ");
+        // DFS 결과 출력
+        for (int node : dfsResult) {
+            bw.write(node + " ");
         }
-        for (int i = 1; i <= N; i++) {
-            bw.write(bfsResult[i] + " ");
-        }
-    }
+        bw.newLine();
 
-    public static void bfs(int node) {
-        
+        // BFS 결과 출력
+        for (int node : bfsResult) {
+            bw.write(node + " ");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
     }
 
     public static void dfs(int node) {
+        dfsVisited[node] = true;
+        dfsResult.add(node);
 
+        for (int neighbor : graph.get(node)) {
+            if (!dfsVisited[neighbor]) {
+                dfs(neighbor);
+            }
+        }
     }
 
+    public static void bfs(int start) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+        bfsVisited[start] = true;
+
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            bfsResult.add(node);
+
+            for (int neighbor : graph.get(node)) {
+                if (!bfsVisited[neighbor]) {
+                    queue.add(neighbor);
+                    bfsVisited[neighbor] = true;
+                }
+            }
+        }
+    }
 }
