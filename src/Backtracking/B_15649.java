f49e4d6f822576e0cd5116c21b8d897dd7191fb5
DEV_WOONG <ywcsuwon@gmail.com>
2025-02-06 17:19:00 +0900

    백준 15659 백트래킹 해결

--- src/Backtracking/B_15649.java
+++ src/Backtracking/B_15649.java
@@ -1,4 +1,45 @@
 package Backtracking;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
 public class B_15649 {
+
+    static int N,M;
+    static int[] arr;
+    static boolean[] visited;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[M];
+        visited = new boolean[N];
+
+        dfs(0);
+    }
+
+    static void dfs(int depth) {
+
+        if (depth == M) {
+            for (int value : arr) {
+                System.out.print(value + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                arr[depth] = i+1;
+                dfs(depth+1);
+                visited[i] = false;
+            }
+        }
+    }
+
 }
