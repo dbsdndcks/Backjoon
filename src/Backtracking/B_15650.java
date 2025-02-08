dc2e8930966336949d11307e345ff061f717b26a
DEV_WOONG <ywcsuwon@gmail.com>
2025-02-08 17:04:01 +0900

    15650중간해결

--- src/Backtracking/B_15650.java
+++ src/Backtracking/B_15650.java
@@ -1,6 +1,25 @@
 package Backtracking;
 
-
-
+import java.io.*;
+import java.util.StringTokenizer;
 public class B_15650 {
+
+    static int N,M;
+
+    public static void dfs(int depth) {
+        
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        // N,M 입력
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        dfs(0);
+    }
+
+
 }
