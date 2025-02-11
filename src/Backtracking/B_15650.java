16cb8974a7cea1e56ff45500a8db89628d6a4e7b
DEV_WOONG <ywcsuwon@gmail.com>
2025-02-11 22:08:06 +0900

    백준 15650 해결

--- src/Backtracking/B_15650.java
+++ src/Backtracking/B_15650.java
@@ -1,25 +1,41 @@
 package Backtracking;
 
-import java.io.*;
-import java.util.StringTokenizer;
+import java.util.Scanner;
+
 public class B_15650 {
 
-    static int N,M;
+    public static int[] arr;
+    public static int N, M;
 
-    public static void dfs(int depth) {
-        
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        N = in.nextInt();
+        M = in.nextInt();
+
+        arr = new int[M];
+
+        dfs(1, 0);
+
     }
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void dfs(int at, int depth) {
 
-        // N,M 입력
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        if (depth == M) {
+            for (int val : arr) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+            return;
+        }
 
-        dfs(0);
+        for (int i = at; i <= N; i++) {
+
+            arr[depth] = i;
+            dfs(i + 1, depth + 1);
+
+        }
     }
-
-
+}
 }
